965aa0bb867767908fd2ac6284bcffc780cb03f9
DaaanielTV <125688359+DaaanielTV@users.noreply.github.com>
2026-05-04 18:53:52 +0200

    test: parametrize aws service default coverage

--- tests/test_aws_service_defaults.py
+++ tests/test_aws_service_defaults.py
@@ -1,6 +1,8 @@
 from pathlib import Path
 import sys
 
+import pytest
+
 sys.path.insert(0, str(Path(__file__).resolve().parents[1]))
 
 import modules.aws_extended_001 as aws001
@@ -27,61 +29,43 @@ def _assert_common_fields(obj, service_name: str, class_index: str, region: str,
     }
 
 
-def test_given_ec2_service_when_initialized_then_applies_ec2_defaults():
-    service = aws001.AWSEC2Service001(region="eu-central-1", account_id="123456789012")
-    _assert_common_fields(service, "ec2", "001", "eu-central-1", "123456789012")
-
-
-def test_given_s3_service_when_initialized_then_applies_s3_defaults():
-    service = aws001.AWSS3Service002(region="us-west-2", account_id="210987654321")
-    _assert_common_fields(service, "s3", "002", "us-west-2", "210987654321")
-
-
-def test_given_rds_service_when_initialized_then_applies_rds_defaults():
-    service = aws001.AWSRDSService003(region="ap-southeast-1", account_id="555544443333")
-    _assert_common_fields(service, "rds", "003", "ap-southeast-1", "555544443333")
-
-
-def test_given_lambda_service_when_initialized_then_applies_lambda_defaults():
-    service = aws001.AWSLambdaService004(region="eu-west-1", account_id="111122223333")
-    _assert_common_fields(service, "lambda", "004", "eu-west-1", "111122223333")
-
-
-def test_given_ecs_service_when_initialized_then_applies_ecs_defaults():
-    service = aws001.AWSECSService005(region="sa-east-1", account_id="444455556666")
-    _assert_common_fields(service, "ecs", "005", "sa-east-1", "444455556666")
-
-
-def test_given_eks_service_when_initialized_then_applies_eks_defaults():
-    service = aws001.AWSEKSService006(region="ca-central-1", account_id="777788889999")
-    _assert_common_fields(service, "eks", "006", "ca-central-1", "777788889999")
-
-
-def test_given_dynamodb_service_when_initialized_then_applies_dynamodb_defaults():
-    service = aws001.AWSDynamoDBService007(region="us-east-2", account_id="101010101010")
-    _assert_common_fields(service, "dynamodb", "007", "us-east-2", "101010101010")
-
-
-def test_given_cloudwatch_service_when_initialized_then_applies_cloudwatch_defaults():
-    service = aws001.AWSCloudWatchService008(region="eu-north-1", account_id="202020202020")
-    _assert_common_fields(service, "cloudwatch", "008", "eu-north-1", "202020202020")
-
-
-def test_given_iam_service_when_initialized_then_applies_iam_defaults():
-    service = aws001.AWSIAMService009(region="ap-northeast-1", account_id="303030303030")
-    _assert_common_fields(service, "iam", "009", "ap-northeast-1", "303030303030")
-
-
-def test_given_vpc_service_when_initialized_then_applies_vpc_defaults():
-    service = aws001.AWSVPCService010(region="af-south-1", account_id="404040404040")
-    _assert_common_fields(service, "vpc", "010", "af-south-1", "404040404040")
-
-
-def test_given_route53_service_when_initialized_then_applies_route53_defaults():
-    service = aws001.AWSRoute53Service011(region="me-south-1", account_id="505050505050")
-    _assert_common_fields(service, "route53", "011", "me-south-1", "505050505050")
-
-
-def test_given_sns_service_when_initialized_then_applies_sns_defaults():
-    service = aws001.AWSSNSService012(region="eu-south-1", account_id="606060606060")
-    _assert_common_fields(service, "sns", "012", "eu-south-1", "606060606060")
+@pytest.mark.parametrize(
+    "service_cls, service_name, class_index, region, account_id",
+    [
+        (aws001.AWSEC2Service001, "ec2", "001", "eu-central-1", "123456789012"),
+        (aws001.AWSS3Service002, "s3", "002", "us-west-2", "210987654321"),
+        (aws001.AWSRDSService003, "rds", "003", "ap-southeast-1", "555544443333"),
+        (aws001.AWSLambdaService004, "lambda", "004", "eu-west-1", "111122223333"),
+        (aws001.AWSECSService005, "ecs", "005", "sa-east-1", "444455556666"),
+        (aws001.AWSEKSService006, "eks", "006", "ca-central-1", "777788889999"),
+        (aws001.AWSDynamoDBService007, "dynamodb", "007", "us-east-2", "101010101010"),
+        (aws001.AWSCloudWatchService008, "cloudwatch", "008", "eu-north-1", "202020202020"),
+        (aws001.AWSIAMService009, "iam", "009", "ap-northeast-1", "303030303030"),
+        (aws001.AWSVPCService010, "vpc", "010", "af-south-1", "404040404040"),
+        (aws001.AWSRoute53Service011, "route53", "011", "me-south-1", "505050505050"),
+        (aws001.AWSSNSService012, "sns", "012", "eu-south-1", "606060606060"),
+    ],
+    ids=[
+        "ec2-eu-central-1",
+        "s3-us-west-2",
+        "rds-ap-southeast-1",
+        "lambda-eu-west-1",
+        "ecs-sa-east-1",
+        "eks-ca-central-1",
+        "dynamodb-us-east-2",
+        "cloudwatch-eu-north-1",
+        "iam-ap-northeast-1",
+        "vpc-af-south-1",
+        "route53-me-south-1",
+        "sns-eu-south-1",
+    ],
+)
+def test_given_service_when_initialized_then_applies_expected_defaults(
+    service_cls,
+    service_name,
+    class_index,
+    region,
+    account_id,
+):
+    service = service_cls(region=region, account_id=account_id)
+    _assert_common_fields(service, service_name, class_index, region, account_id)
